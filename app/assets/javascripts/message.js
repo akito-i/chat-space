59d294ed5284798180830b73853a595044987946
akito-i <a-imamura0827@gmail.com>
2020-06-20 11:25:20 +0900

    auto message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,6 @@
 $(function(){ 
+  var last_message_id = $('.message:last').data("message-id");
+  console.log(last_message_id);
   function buildHTML(message){
    if ( message.image ) {
      var html =
@@ -64,4 +66,30 @@ $('#new_message').on('submit', function(e){
     $(".send-btn").prop("disabled", false);
   })
  })
+ var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    }
+    console.log('success');
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
 });
