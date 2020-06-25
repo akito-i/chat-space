d05f42aaa21cd26e86de77b7efadb744c51c51b3
akito-i <a-imamura0827@gmail.com>
2020-06-25 20:38:15 +0900

    a

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,38 +2,36 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
+     `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <div class="lower-message__content">
+            ${message.content}
+          </div>
+        </div>
+        <img src=${message.image} >
+      </div>`
      return html;
    } else {
      var html =
-      `<div class="message">
-         <div class="upper-message">
-           <div class="upper-message__user-name">
+      `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
              ${message.user_name}
-           </div>
-           <div class="upper-message__date">
+            <div class="upper-message__date">
              ${message.created_at}
-           </div>
-         </div>
+            </div>
+          </div>
          <div class="lower-message">
-           <p class="lower-message__content">
+           <div class="lower-message__content">
              ${message.content}
-           </p>
+           </div>
          </div>
        </div>`
      return html;
